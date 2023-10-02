402dd332689b4938b28c284316e40f8fc8feaa36
Vladyslav PC Kyiv <prosto.acc.vlada@gmail.com>
2023-10-02 17:47:37 +0300

    DB changed to PostgreSQL virt_env

--- tie_django_learning/settings.py
+++ tie_django_learning/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'tie_django_learning.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'money_operations_db',
+        'USER': 'postgres',
+        'PASSWORD': '3468',
+        'HOST': 'localhost',
     }
 }
 
